e50c11904c6f62dede07bbcabe911c9ab66e26f9
clarinharocha <clararocha.s30@gmail.com>
2025-06-04 11:07:43 -0400

    Teste

---
+++ src/routes/03/external/posts/[id]/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    let { data } = $props();
+    console.log(data)
+</script>
+<b>ID do usuário:</b> {data.post.userId}
+<br />
+<b>ID da postagem:</b> {data.post.id}
+<br />
+<b>Título:</b> {data.post.title}
+<br />
+<b>Conteúdo:</b> {data.post.body}
+<br />
+<b>Comentários:</b>
+{#each data.comments as comment}
+    {comment.name}
+    <br />
+    {comment.body}
+    <br />
+    {comment.email}
+{/each}
