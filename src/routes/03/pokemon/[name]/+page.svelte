6a3cb0eb5fc60f162acdfcc4c6ca64dc271b7dde
clarinharocha <clararocha.s30@gmail.com>
2025-06-04 12:27:22 -0400

    qualquer coisa

---
+++ src/routes/03/pokemon/[name]/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    let { data } = $props();
+</script>
+
+<b>{data.pokemon.name.toUpperCase()}</b>
+
+<br />
+
+<img src={data.pokemon.sprites.front_default} alt={data.pokemon.name} />
+
+<br />
+
+{data.pokemon.height / 10 }  m
+
+<br />
+
+{data.pokemon.weight / 10 } kg
+
+<br />
+
+{#each data.pokemon.types as type}
+    <span class="badge bg-secondary text-capitalize">{type.type.name}</span>
+{/each}
+
+<br />
+
+{#each data.pokemon.abilities as ability}
+    {ability.ability.name}
+    <br />
+{/each}
